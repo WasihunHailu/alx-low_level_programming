f9d382a4968aa37b876c552e24ac40ed2210af47
WasihunHailu <hailuwasihun@gmail.com>
2022-12-12 06:25:47 -0800

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * Return: Always 0 (suceess)
+ */
+int main(void)
+{
+	char x;
+
+	for (x = 'a'; x <= 'z'; x++)
+		putchar(x);
+	putchar('\n');
+	return (0);
+}
