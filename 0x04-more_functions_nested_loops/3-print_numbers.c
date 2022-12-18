924b2a5e9f532568d077a675cd3ffcf101945ed6
WasihunHailu <hailuwasihun@gmail.com>
2022-12-18 10:39:36 -0800

    updated

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_numbers - prints 0 to 9.
+ * Return: nothing
+ */
+void print_numbers(void)
+{
+	int num = 0;
+
+	while (num < 10)
+	{
+		_putchar('0' + num);
+		num++;
+	}
+	_putchar('\n');
+}
